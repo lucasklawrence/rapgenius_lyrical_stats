c24c6e93dd2c4e4f401a76610f320cd2e440ff21
lucasklawrence <47094651+lucasklawrence@users.noreply.github.com>
2019-05-29 17:22:28 -0400

    Update artist_word_cloud.py
    remove unused functions, change variable names to not shadow outerscope

--- artist_word_cloud.py
+++ artist_word_cloud.py
@@ -9,20 +9,12 @@ import wordcloud
 import matplotlib.pyplot as plt
 from collections import Counter
 
-def request_song_info(song_title, artist_name):
+
+def find_artist_url(search_artist):
     base_url = 'https://api.genius.com'
     headers = {'Authorization': 'Bearer ' + '071qS0OFRPZ8HNpnv_D8XeVCIFJLxIoc287fimUGIlAKciRrQlDkaKUIvbH15p_C'}
     search_url = base_url + '/search'
-    data = {'q': song_title + ' ' + artist_name}
-    response = requests.get(search_url, data=data, headers=headers)
-
-    return response
-
-def find_artist_url(artist):
-    base_url = 'https://api.genius.com'
-    headers = {'Authorization': 'Bearer ' + '071qS0OFRPZ8HNpnv_D8XeVCIFJLxIoc287fimUGIlAKciRrQlDkaKUIvbH15p_C'}
-    search_url = base_url + '/search'
-    data = {'q': artist}
+    data = {'q': search_artist}
     response = requests.get(search_url, data=data, headers=headers)
 
     json = response.json()
@@ -40,16 +32,16 @@ def find_artist_url(artist):
     possible_artist_urls = set()
     for hit in hits:
         result = hit['result']
-        artist = result['primary_artist']
-        url = artist['url']
+        primary_artist = result['primary_artist']
+        url = primary_artist['url']
         if url not in possible_artist_urls:
             possible_artist_urls.add(url)
 
     return possible_artist_urls
 
 
-def get_artist_albums_from_artist_url(artist_url):
-    html = requests.get(artist_url)
+def get_artist_albums_from_artist_url(artist_link):
+    html = requests.get(artist_link)
     data = html.text
     soup = BeautifulSoup(data, "html.parser")
 
@@ -63,26 +55,26 @@ def get_artist_albums_from_artist_url(artist_url):
     return album_urls
 
 
-def get_song_urls_from_album_url(album_url):
-    html = requests.get(album_url)
+def get_song_urls_from_album_url(album_link):
+    html = requests.get(album_link)
     data = html.text
     soup = BeautifulSoup(data, "html.parser")
 
-    song_urls = set()
+    song_genius_urls = set()
     for link in soup.find_all('a'):
         possible_link = link.get('href')
 
-        ## this gets all possible songs from the album
+        # this gets all possible songs from the album
         if re.search("-lyrics", possible_link):
-            song_url = possible_link.lower()
-            if song_url not in song_urls:
+            song_genius_url = possible_link.lower()
+            if song_genius_url not in song_genius_urls:
                 song_urls.add(song_url)
 
     return song_urls
 
 
-def get_lyrics_from_song_url(song_url):
-    html = requests.get(song_url)
+def get_lyrics_from_song_url(song_link):
+    html = requests.get(song_link)
     data = html.text
     soup = BeautifulSoup(data, "html.parser")
 
@@ -124,14 +116,12 @@ if __name__ == '__main__':
     flag = 0
     for album in artist.get_albums():
         album.set_word_count_from_songs()
-        #sprint(album.get_word_count())
         lyrics = ""
         for song in album.get_songs():
             if song.get_lyrics() is not None:
                 lyrics += song.get_lyrics()
         words = lyrics.lower().split()
         wordCount = Counter(words)
-        #print(wordCount)
 
     artist.set_word_count_from_albums()
 
@@ -140,49 +130,3 @@ if __name__ == '__main__':
     plt.imshow(artist_word_cloud, interpolation='bilinear')
     plt.axis("off")
     plt.show()
-    #print(artist.get_word_count())
-
-"""
-    flag = 0
-    for link in test:
-        albums = get_artist_albums_from_artist_url(link)
-        for album in albums:
-            album_songs = get_song_urls_from_album_url(album)
-            for song in album_songs:
-                if flag == 0:
-                    get_lyrics_from_song_url(song)
-                    flag = 1
-
-
-# change the JSON string into a JSON object
-#jsonObject = json.loads(json)
-
-# print the keys and values
-for key in json:
-    value = json[key]
-    for item in value:
-        print(value[item])
-    print("The key and value are ({}) = ({})".format(key, value))
-remote_song_info = None
-
-wow = requests.get('https://genius.com/search?q=', data='Drake')
-print(wow.text)
-#for hit in json['response']['hits']:
-#    if artist_name.lower() in hit['result']['primary_artist']['name'].lower():
-#       remote_song_info = hit
-#        break
-
-print(remote_song_info)
-#print(session)
-#rap_genius.get_access_token()
-#rap_genius.get_access_token(str='POST', decoder=parse_utf8_qsl, key=s)
-
-#https://api.genius.com/oauth/authorize?
-#client_id=YOUR_CLIENT_ID&
-#redirect_uri=YOUR_REDIRECT_URI&
-#scope=REQUESTED_SCOPE&
-#state=SOME_STATE_VALUE&
-#response_type=code
-
-#print(request_token)
-"""
