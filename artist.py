1c016b6039e140879d5ef0105e88f7d6af75f7c9
lucasklawrence <47094651+lucasklawrence@users.noreply.github.com>
2019-06-02 18:40:28 -0400

    restructed code
    moved functions to artist, album, song, class
    
    created stats object
    
    create dictionary without certain parts of speech (should be more meaningful)

--- artist.py
+++ artist.py
@@ -1,12 +1,87 @@
+# web imports
+import requests
+from bs4 import BeautifulSoup
+
+# regular expression and string to find lyrics / parts of lyrics
+import re
+import string
+
+# datetime to print time for each album
+import datetime as datetime
+
+
+class Stats:
+    def __init__(self, word_count):
+        self.word_count = word_count
+        self.starting_letter_count = self.init_starting_letter_count()
+
+        self.remove = {"and", "im", "or",
+                          # pronouns
+                          "i", "me", "my", "mine", "myself",
+                          "you", "your", "yours", "yourself",
+                          "he", "him", "his", "himself",
+                          "she", "her", "hers", "herself",
+                          "it", "its", "itself",
+                          "we", "us", "our", "ours", "ourselves",
+                          "they", "them", "their", "theirs"
+        
+                          # articles
+                          "a", "the", "an",
+                          # prepositions
+                          "at", "in", "by", "for", "on", "to", "from", "of", "with"}
+
+        self.word_count_minus = self.init_word_count_minus()
+
+    def get_word_count(self):
+        return self.word_count
+
+    def set_word_count(self, word_count):
+        self.word_count = word_count
+
+    def init_starting_letter_count(self):
+        starting_letter_count = {}
+        for key in self.word_count:
+            letter = key[0]
+            if letter not in starting_letter_count:
+                starting_letter_count[letter] = 1
+            else:
+                prev_count = starting_letter_count[letter]
+                starting_letter_count[letter] = prev_count + 1
+
+        return starting_letter_count
+
+    def get_starting_letter_count(self):
+        return self.starting_letter_count
+
+    def get_word_count_minus(self):
+        return self.word_count_minus
+
+    def init_word_count_minus(self):
+        word_count_minus = {}
+        for key in self.word_count:
+            if key not in self.remove:
+                word_count_minus[key] = self.word_count[key]
+
+        return word_count_minus
+
 
 class Artist:
     def __init__(self, artist_name, artist_url):
+        """
+        :param artist_name: Name of music artist
+        :param artist_url: Rap genius url of music artist
+        """
         self.artist_name = artist_name
         self.artist_url = artist_url
+
+        # initialize albums from artist url
         self.albums = list()
-        self.wordCount = {}
+        self.init_albums()
+
+        self.stats = self.init_stats()
         self.wordCloud = None
-        self.startingLetterCount = {}
+        # dictionary for word count with parts of speech removed
+        self.wordCloudMinusPartsOfSpeech = None
 
     def get_artist_name(self):
         return self.artist_name
@@ -23,23 +98,43 @@ class Artist:
     def get_albums(self):
         return self.albums
 
+    def init_albums(self):
+        # Get Albums from Artist
+        genius_album_urls = get_artist_albums_from_artist_url(self.get_artist_url())
+
+        # create albums for artist
+        for genius_album_url in genius_album_urls:
+            init_partition = str.partition(genius_album_url, "/albums/")
+            album_name = (str.partition(init_partition[2], "/"))[2]
+            album_name = str.replace(album_name, "-", " ")
+            genius_album = Album(album_name, genius_album_url)
+            self.add_album(genius_album)
+
     def add_album(self, album):
         self.albums.append(album)
 
+    def get_stats(self):
+        return self.stats
+
+    def init_stats(self):
+        combined_words = {}
+
+        for album in self.get_albums():
+            album_stats = album.get_stats()
+            if album_stats is not None:
+                album_word_count = album_stats.get_word_count()
+                if album_word_count is not None:
+                    for key in album_word_count:
+                        if key not in combined_words:
+                            combined_words[key] = album_word_count[key]
+                        else:
+                            prev_count = combined_words[key]
+                            combined_words[key] = prev_count + album_word_count[key]
+
+        return Stats(combined_words)
+
     def get_word_count(self):
         return self.wordCount
-
-    def set_word_count_from_albums(self):
-        for album in self.get_albums():
-            if album.get_word_count() is None:
-                album.set_word_count_from_songs()
-
-            for key in album.wordCount:
-                if key not in self.wordCount:
-                    self.wordCount[key] = album.wordCount[key]
-                else:
-                    previous_count = self.wordCount[key]
-                    self.wordCount[key] = previous_count + album.wordCount[key]
 
     def get_word_cloud(self):
         return self.wordCloud
@@ -47,27 +142,28 @@ class Artist:
     def set_word_cloud(self, wordcloud):
         self.wordCloud = wordcloud
 
-    def get_starting_letter_count(self):
-        return self.startingLetterCount
+    def get_word_cloud_minus(self):
+        return self.wordCloudMinusPartsOfSpeech
 
-    def set_starting_letter_count_from_word_count(self):
-        for key in self.wordCount:
-            letter = key[0]
-            if letter not in self.startingLetterCount:
-                self.startingLetterCount[letter] = 1
-            else:
-                prev_count = self.startingLetterCount[letter]
-                self.startingLetterCount[letter] = prev_count + 1
+    def set_word_cloud_minus(self, wordcloud):
+        self.wordCloudMinusPartsOfSpeech = wordcloud
 
 
 class Album:
     def __init__(self, album_name, album_url):
         self.album_name = album_name
         self.album_url = album_url
+
+        # initialize songs
         self.songs = list()
-        self.wordCount = {}
+        self.init_songs()
+
+        self.stats = self.init_stats()
         self.wordCloud = None
-        self.startingLetterCount = {}
+
+        # dictionary for word count with parts of speech removed
+        self.wordCountMinusPartsOfSpeech = {}
+        self.wordCloudMinusPartsOfSpeech = None
 
     def get_album_name(self):
         return self.album_name
@@ -84,23 +180,40 @@ class Album:
     def get_songs(self):
         return self.songs
 
+    def init_songs(self):
+        # create songs for album
+        song_urls = get_song_urls_from_album_url(self.get_album_url())
+        print("Scraping lyrics for album: " + self.get_album_name())
+        initial_time = datetime.datetime.now()
+        for song_url in song_urls:
+            song_title = (str.partition(song_url, "genius.com/"))[2]
+            current_song = Song(song_title, song_url)
+            self.add_song(current_song)
+        end_time = datetime.datetime.now()
+        elapsed_time = (end_time - initial_time).total_seconds()
+        print("Finished in: " + str(elapsed_time) + " seconds")
+
     def add_song(self, song):
         self.songs.append(song)
 
-    def get_word_count(self):
-        return self.wordCount
+    def get_stats(self):
+        return self.stats
 
-    def set_word_count_from_songs(self):
+    def init_stats(self):
+        combined_words = {}
         for song in self.get_songs():
-            if song.get_word_count() is None:
-                song.set_word_count_from_lyrics()
+            song_stats = song.get_stats()
+            if song_stats is not None:
+                song_word_count = song_stats.get_word_count()
+                if song_word_count is not None:
+                    for key in song_word_count:
+                        if key not in combined_words:
+                            combined_words[key] = song_word_count[key]
+                        else:
+                            prev_count = combined_words[key]
+                            combined_words[key] = prev_count + song_word_count[key]
 
-            for key in song.wordCount:
-                if key not in self.wordCount:
-                    self.wordCount[key] = song.wordCount[key]
-                else:
-                    previous_count = self.wordCount[key]
-                    self.wordCount[key] = previous_count + song.wordCount[key]
+        return Stats(combined_words)
 
     def get_word_cloud(self):
         return self.wordCloud
@@ -108,24 +221,20 @@ class Album:
     def set_word_cloud(self, wordcloud):
         self.wordCloud = wordcloud
 
-    def get_starting_letter_count(self):
-        return self.startingLetterCount
+    def get_word_cloud_minus(self):
+        return self.wordCloudMinusPartsOfSpeech
 
-    def set_starting_letter_count_from_word_count(self):
-        for key in self.wordCount:
-            letter = key[0]
-            if letter not in self.startingLetterCount:
-                self.startingLetterCount[letter] = 1
-            else:
-                prev_count = self.startingLetterCount[letter]
-                self.startingLetterCount[letter] = prev_count + 1
+    def set_word_cloud_minus(self, wordcloud):
+        self.wordCloudMinusPartsOfSpeech = wordcloud
+
 
 class Song:
     def __init__(self, song_name, song_url):
         self.song_name = song_name
         self.song_url = song_url
-        self.lyrics = None
-        self.wordCount = {}
+        self.lyrics = self.init_lyrics()
+        self.wordCount = self.init_word_count()
+        self.stats = self.init_stats()
 
     def get_song_name(self):
         return self.song_name
@@ -145,17 +254,124 @@ class Song:
     def set_lyrics(self, lyrics):
         self.lyrics = lyrics
 
+    def init_lyrics(self):
+        html = requests.get(self.get_song_url())
+        data = html.text
+        soup = BeautifulSoup(data, "html.parser")
+        lyrics_html = soup.find("div", {"class": "lyrics"})
+
+        song_lyrics = None
+        if lyrics_html is not None:
+            song_lyrics = lyrics_html.get_text()
+
+            # remove [Chorus: Artist 1] [Verse: Artist 2], etc
+            song_lyrics = remove_items_in_brackets(song_lyrics)
+            # removes punctuation
+            song_lyrics = song_lyrics.translate(str.maketrans('', '', string.punctuation))
+
+        return song_lyrics
+
+    def get_stats(self):
+        return self.stats
+
+    def init_stats(self):
+        return Stats(self.get_word_count())
+
     def get_word_count(self):
         return self.wordCount
 
-    def set_word_count_from_lyrics(self):
+    def init_word_count(self):
+        word_count = {}
         if self.lyrics is None:
             return
 
         words = self.lyrics.lower().split()
         for word in words:
-            if word not in self.wordCount:
-                self.wordCount[word] = 1
+            if word not in word_count:
+                word_count[word] = 1
             else:
-                prev_count = self.wordCount[word]
-                self.wordCount[word] = prev_count + 1
+                prev_count = word_count[word]
+                word_count[word] = prev_count + 1
+
+        return word_count
+
+
+# Utilities
+
+def get_artist_albums_from_artist_url(artist_link):
+    html = requests.get(artist_link)
+    data = html.text
+    soup = BeautifulSoup(data, "html.parser")
+
+    album_urls = set()
+    for link in soup.find_all('a'):
+        possible_link = link.get('href')
+        if re.match('https://', possible_link) and re.search('/albums/', possible_link):
+            if possible_link.lower() not in album_urls:
+                album_urls.add(possible_link.lower())
+
+    return album_urls
+
+
+def get_song_urls_from_album_url(album_link):
+    html = requests.get(album_link)
+    data = html.text
+    soup = BeautifulSoup(data, "html.parser")
+
+    song_genius_urls = set()
+    for link in soup.find_all('a'):
+        possible_link = link.get('href')
+        # this gets all possible songs from the album
+        if possible_link is not None and re.search("-lyrics", possible_link):
+            song_genius_url = possible_link.lower()
+            if song_genius_url not in song_genius_urls:
+                song_genius_urls.add(song_genius_url)
+
+    return song_genius_urls
+
+
+def get_lyrics_from_song_url(song_link):
+    html = requests.get(song_link)
+    data = html.text
+    soup = BeautifulSoup(data, "html.parser")
+    lyrics_html = soup.find("div", {"class": "lyrics"})
+
+    song_lyrics = None
+    if lyrics_html is not None:
+        song_lyrics = lyrics_html.get_text()
+
+        # remove [Chorus: Artist 1] [Verse: Artist 2], etc
+        song_lyrics = remove_items_in_brackets(song_lyrics)
+        # removes punctuation
+        song_lyrics = song_lyrics.translate(str.maketrans('', '', string.punctuation))
+    return song_lyrics
+
+
+def remove_items_in_brackets(lyrics):
+    initial_indices = list()
+    end_indices = list()
+
+    # get starting and ending points of brackets in string
+    for index in range(len(lyrics)):
+        if lyrics[index] == '[':
+            initial_indices.append(index)
+        if lyrics[index] == ']':
+            end_indices.append(index)
+
+    initial_length = len(initial_indices)
+    end_length = len(end_indices)
+    remove = list()
+
+    # get strings that are in brackets to replace with empty strings
+    if initial_length == end_length:
+        for index in range(initial_length):
+            initial_index = initial_indices[index]
+            end_index = end_indices[index]
+            remove.append(lyrics[initial_index:end_index + 1])
+
+    # replace strings w/ empty string
+    for item in remove:
+        lyrics = lyrics.replace(item, "")
+
+    return lyrics
+
--- artist_word_cloud.py
+++ artist_word_cloud.py
@@ -1,20 +1,12 @@
 # web imports
 import requests
-from bs4 import BeautifulSoup
-
-# regular expression and string to find lyrics / parts of lyrics
-import re
-import string
 
 # import classes
-from artist import Artist, Album, Song
+from artist import Artist, Album, Song, Stats
 
 # wordcloud and plot wordcloud
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
-
-# datetime to print time for each album
-import datetime as datetime
 
 
 def find_artist_url(search_artist):
@@ -47,138 +39,52 @@ def find_artist_url(search_artist):
     return possible_artist_urls
 
 
-def get_artist_albums_from_artist_url(artist_link):
-    html = requests.get(artist_link)
-    data = html.text
-    soup = BeautifulSoup(data, "html.parser")
+def remove_common_parts_of_speech(word_count):
+    new_dictionary = {}
+    remove = {"and", "im", "or",
+              # pronouns
+              "i", "me", "my", "mine", "myself",
+              "you", "your", "yours", "yourself",
+              "he", "him", "his", "himself",
+              "she", "her", "hers", "herself",
+              "it", "its", "itself",
+              "we", "us", "our", "ours", "ourselves",
+              "they", "them", "their", "theirs"
+                                         
+              # articles
+              "a", "the", "an",
+              # prepositions
+              "at", "in", "by", "for", "on", "to", "from", "of", "with"}
 
-    album_urls = set()
-    for link in soup.find_all('a'):
-        possible_link = link.get('href')
-        if re.match('https://', possible_link) and re.search('/albums/', possible_link):
-            if possible_link.lower() not in album_urls:
-                album_urls.add(possible_link.lower())
+    for key in word_count:
+        if key not in remove:
+            new_dictionary[key] = word_count[key]
 
-    return album_urls
-
-
-def get_song_urls_from_album_url(album_link):
-    html = requests.get(album_link)
-    data = html.text
-    soup = BeautifulSoup(data, "html.parser")
-
-    song_genius_urls = set()
-    for link in soup.find_all('a'):
-        possible_link = link.get('href')
-        # this gets all possible songs from the album
-        if possible_link is not None and re.search("-lyrics", possible_link):
-            song_genius_url = possible_link.lower()
-            if song_genius_url not in song_genius_urls:
-                song_genius_urls.add(song_genius_url)
-
-    return song_genius_urls
-
-
-def get_lyrics_from_song_url(song_link):
-    html = requests.get(song_link)
-    data = html.text
-    soup = BeautifulSoup(data, "html.parser")
-    lyrics_html = soup.find("div", {"class": "lyrics"})
-
-    song_lyrics = None
-    if lyrics_html is not None:
-        song_lyrics = lyrics_html.get_text()
-
-        # remove [Chorus: Artist 1] [Verse: Artist 2], etc
-        song_lyrics = remove_items_in_brackets(song_lyrics)
-        # removes punctuation
-        song_lyrics = song_lyrics.translate(str.maketrans('', '', string.punctuation))
-    return song_lyrics
-
-
-def remove_items_in_brackets(lyrics):
-    initial_indices = list()
-    end_indices = list()
-
-    # get starting and ending points of brackets in string
-    for index in range(len(lyrics)):
-        if lyrics[index] == '[':
-            initial_indices.append(index)
-        if lyrics[index] == ']':
-            end_indices.append(index)
-
-    initial_length = len(initial_indices)
-    end_length = len(end_indices)
-    remove = list()
-
-    # get strings that are in brackets to replace with empty strings
-    if initial_length == end_length:
-        for index in range(initial_length):
-            initial_index = initial_indices[index]
-            end_index = end_indices[index]
-            remove.append(lyrics[initial_index:end_index + 1])
-
-    # replace strings w/ empty string
-    for item in remove:
-        lyrics = lyrics.replace(item, "")
-
-    return lyrics
-
-
-def add_albums_to_artist(artist):
-    # Get Albums from Artist
-    genius_album_urls = get_artist_albums_from_artist_url(artist.get_artist_url())
-
-    # create albums for artist
-    for genius_album_url in genius_album_urls:
-        init_partition = str.partition(genius_album_url, "/albums/")
-        album_name = (str.partition(init_partition[2], "/"))[2]
-        album_name = str.replace(album_name, "-", " ")
-        genius_album = Album(album_name, genius_album_url)
-        artist.add_album(genius_album)
-
-
-def add_songs_to_artist_albums(artist):
-    # create songs for album
-    for album_iter in artist.get_albums():
-        song_urls = get_song_urls_from_album_url(album_iter.get_album_url())
-        print("Scraping lyrics for album: " + album_iter.get_album_name())
-        initial_time = datetime.datetime.now()
-        for song_url in song_urls:
-            song_title = (str.partition(song_url, "genius.com/"))[2]
-            current_song = Song(song_title, song_url)
-            current_song.set_lyrics(get_lyrics_from_song_url(current_song.get_song_url()))
-            current_song.set_word_count_from_lyrics()
-            album_iter.add_song(current_song)
-        album_iter.set_word_count_from_songs()
-        album_iter.set_starting_letter_count_from_word_count()
-        end_time = datetime.datetime.now()
-        elapsed_time = (end_time - initial_time).total_seconds()
-        print("Finished in: " + str(elapsed_time) + " seconds")
-    artist.set_word_count_from_albums()
-    artist.set_starting_letter_count_from_word_count()
-
-
-def initialize_albums_and_songs(artist):
-    add_albums_to_artist(artist)
-    add_songs_to_artist_albums(artist)
+    return new_dictionary
 
 
 def create_word_clouds_for_artist(artist):
 
-    artist_word_count = artist.get_word_count()
-    artist_starting_letter_count = artist.get_starting_letter_count()
-
+    artist_word_count = artist.get_stats().get_word_count()
+    artist_word_count_minus = artist.get_stats().get_word_count_minus()
     if len(artist_word_count) != 0:
         artist_word_cloud = WordCloud().generate_from_frequencies(artist_word_count)
         artist.set_word_cloud(artist_word_cloud)
 
+    if len(artist_word_count_minus) != 0:
+        artist_word_cloud_minus = WordCloud().generate_from_frequencies(artist_word_count_minus)
+        artist.set_word_cloud_minus(artist_word_cloud_minus)
+
     # set word clouds for each album
     for artist_album in artist.get_albums():
-        album_word_count = artist_album.get_word_count()
+        album_word_count = artist_album.get_stats().get_word_count()
+        album_word_count_minus = artist_album.get_stats().get_word_count_minus()
         if len(album_word_count) != 0:
             album_word_cloud = WordCloud().generate_from_frequencies(album_word_count)
             artist_album.set_word_cloud(album_word_cloud)
+        if len(album_word_count_minus) != 0:
+            album_word_cloud_minus = WordCloud().generate_from_frequencies(album_word_count_minus)
+            artist_album.set_word_cloud_minus(album_word_cloud_minus)
 
 
 if __name__ == '__main__':
@@ -207,7 +113,6 @@ if __name__ == '__main__':
             artist_name = str.partition(artist_url, "/artists/")[2]
             rap_genius_artist = Artist(artist_name, artist_url)
 
-            initialize_albums_and_songs(rap_genius_artist)
             create_word_clouds_for_artist(rap_genius_artist)
             # print out choices and ask user to select which word cloud to present
             print("A: " + rap_genius_artist.get_artist_name() + " Word Cloud")
@@ -230,7 +135,7 @@ if __name__ == '__main__':
                     next_choice = input("Select a Word Cloud to display: ")
 
                 if next_choice == 'A':
-                    plt.imshow(rap_genius_artist.get_word_cloud(), interpolation='bilinear')
+                    plt.imshow(rap_genius_artist.get_word_cloud_minus(), interpolation='bilinear')
                     plt.title(rap_genius_artist.get_artist_name())
                     plt.axis("off")
                     plt.show()
